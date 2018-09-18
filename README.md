10a48edeb3ab7636158790318ecb363661d2c74a
nineSean <ninesean1989@gmail.com>
2018-09-18 13:21:05 +0800

    add simple todolist feature

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## Introduce
 
   make todo list with Vue.js
+  
+## Usage
+
+  打开`index.html`或者起一个服务器
+  
+  
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
-import test from './test.js'
+import Vue from 'vue'
+// Vue.config.devtools = true
 
-console.log(test())
+
+let app = new Vue({
+  el: '#app',
+  data: {
+    index: 0,
+    newTodo: '',
+    todoList: []
+  },
+  created: function(){
+    console.log(this['index'], this.index)
+    window.onbeforeunload = () => {
+      this.setStorage('newTodo')
+      this.setStorage('todoList')
+      this.setStorage('index')
+    }
+    this.index = this.getStorage('index') || 0
+    this.newTodo = this.getStorage('newTodo') || ''
+    this.todoList = this.getStorage('todoList') || []
+  },
+  methods: {
+    addTodo: function () {
+      this.newTodo === ''
+        ? console.log('please type something')
+        : this
+          .todoList
+          .push({
+            key: this.index++,
+            done: false,
+            createdAt: new Date().toLocaleDateString(),
+            content: this.newTodo
+          })
+      this.newTodo = ''
+    },
+    removeTodo: function(todo){
+      let index = this.todoList.indexOf(todo)
+      this.todoList.splice(index, 1)
+    },
+    setStorage: function(name){
+      window.localStorage.setItem(name, JSON.stringify(this[name]))
+    },
+    getStorage: function(name){
+      return JSON.parse(window.localStorage.getItem(name))
+    }
+  },
+})
+
