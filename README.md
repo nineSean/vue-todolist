09172a65aaa7b96253a358256365ee3ca0e260a8
nineSean <ninesean1989@gmail.com>
2018-09-18 17:49:17 +0800

    add login logout sign up with leancloud

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Introduce
 
-  make todo list with Vue.js
+  - make todo list with Vue.js
+  - <a href="https://ninesean.github.io/vue-todolist/index.html">预览效果</a>
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import Vue from 'vue'
-// Vue.config.devtools = true
+import AV from 'leancloud-storage'
 
+var APP_ID = 'mYh0wItrTJIa21cOI97cqHdN-gzGzoHsz';
+var APP_KEY = 'RvNjHyG3CLokYoLDRoXWvF3o';
+AV.init({appId: APP_ID, appKey: APP_KEY})
 
 let app = new Vue({
   el: '#app',
   data: {
+    actionType: 'login',
+    formData: {
+      username: '',
+      password: ''
+    },
+    isLogined: false,
+    account: '',
     index: 0,
     newTodo: '',
     todoList: []
   },
-  created: function(){
-    console.log(this['index'], this.index)
+  created: function () {
     window.onbeforeunload = () => {
       this.setStorage('newTodo')
       this.setStorage('todoList')
@@ -21,6 +30,40 @@ let app = new Vue({
     this.todoList = this.getStorage('todoList') || []
   },
   methods: {
+    login: function () {
+      let app = this
+      AV.User.logIn(this.formData.username, this.formData.password).then(function (loginedUser) {
+        console.log(loginedUser)
+
+        app.isLogined = true
+        app.account = loginedUser.attributes.username
+      }, function (error) {
+        console.log(error)
+      });
+    },
+    signUp: function () {
+      let app = this
+      let user = new AV.User();
+      user.setUsername(this.formData.username);
+      user.setPassword(this.formData.password);
+      user
+        .signUp()
+        .then(function (loginedUser) {
+          app.login()
+        }, (function (error) {
+          console.log(error)
+        }));
+    },
+    logout: function(){
+      AV.User.logOut()
+      this.isLogined = false
+      app.initInfo()
+    },
+    initInfo: function () {
+      this.formData.username = ''
+      this.formData.password = ''
+      this.actionType = 'login'
+    },
     addTodo: function () {
       this.newTodo === ''
         ? console.log('please type something')
@@ -34,16 +77,21 @@ let app = new Vue({
           })
       this.newTodo = ''
     },
-    removeTodo: function(todo){
-      let index = this.todoList.indexOf(todo)
-      this.todoList.splice(index, 1)
+    removeTodo: function (todo) {
+      let index = this
+        .todoList
+        .indexOf(todo)
+      this
+        .todoList
+        .splice(index, 1)
     },
-    setStorage: function(name){
-      window.localStorage.setItem(name, JSON.stringify(this[name]))
+    setStorage: function (name) {
+      window
+        .localStorage
+        .setItem(name, JSON.stringify(this[name]))
     },
-    getStorage: function(name){
+    getStorage: function (name) {
       return JSON.parse(window.localStorage.getItem(name))
     }
-  },
+  }
 })
-
